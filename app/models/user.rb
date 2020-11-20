fefa3c3129105e4120c0313e0ad01fd3055486ea
katsu-piko <katsudon0064@gmail.com>
2020-11-20 19:12:21 +0900

    アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  has_many :tweets
+
   validates :nickname, presence: true, length: { maximum: 6 }
 end

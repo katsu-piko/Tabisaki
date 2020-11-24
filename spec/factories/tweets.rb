160ea78f3191140daf525f5936f76cfb92a3c720
katsu-piko <katsudon0064@gmail.com>
2020-11-24 12:02:10 +0900

    tweetモデル単体テストコード

--- spec/factories/tweets.rb
+++ spec/factories/tweets.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :tweet do
+    after(:build) do |tweet|
+      tweet.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    spot {Faker::Lorem.characters(number: 16)}
+    comment {Faker::Lorem.sentence}
+    advice {Faker::Lorem.sentence}
+    area_id { Faker::Number.between(from: 2, to: 49) }
+    association :user
   end
 end
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @tweet = FactoryBot.build(:tweet)
+    end
+
+    describe 'スポット投稿' do
+      context '投稿ができる場合' do
+        it 'image,spot,comment,advice,area_idが存在していれば投稿できること' do
+          expect(@tweet).to be_valid
+        end
+      end
+      context '投稿ができない場合' do
+        it 'imageが空では投稿できないこと' do
+          @tweet.image = nil
+          @tweet.valid?
+          expect(@tweet.errors.full_messages).to include("Image can't be blank")
+        end
+        it 'spotが空では投稿できないこと' do
+          @tweet.spot = nil
+          @tweet.valid?
+          expect(@tweet.errors.full_messages).to include("Spot can't be blank")
+        end
+        it 'spotが16文字以上では投稿できないこと' do
+          @tweet.spot = "とうきょうとえどとうきょうはくぶつかん"
+          @tweet.valid?
+          expect(@tweet.errors.full_messages).to include('Spot is too long (maximum is 16 characters)')
+        end
+        it 'commentが空では投稿できないこと' do
+          @tweet.comment = nil
+          @tweet.valid?
+          expect(@tweet.errors.full_messages).to include("Comment can't be blank")
+        end
+        it 'adviceが空では投稿できないこと' do
+          @tweet.advice = nil
+          @tweet.valid?
+          expect(@tweet.errors.full_messages).to include("Advice can't be blank")
+        end
+        it 'area_idが空では投稿できないこと' do
+          @tweet.area_id = nil
+          @tweet.valid?
+          expect(@tweet.errors.full_messages).to include("Area can't be blank", "Area is not a number")
+        end
+        it 'area_idが1では投稿できないこと' do
+          @tweet.area_id = 1
+          @tweet.valid?
+          expect(@tweet.errors.full_messages).to include("Area must be other than 1")
+        end
+      end
+    end
+  end
 end

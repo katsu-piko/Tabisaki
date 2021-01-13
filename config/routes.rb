73167e0625f64d7ed940438ce341d178a93989a3
katsu-piko <katsudon0064@gmail.com>
2021-01-13 13:54:23 +0900

    コード修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'favorites/create'
-  get 'favorites/destroy'
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
